b8eac943ba489ccd81d7b23ddacc77eaed07d4ce
Robert Martinez <rdmarenco@uninorte.edu.co>
2024-04-03 16:14:42 -0500

    Set spider arguments via env vars

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import os
+
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 
 # This could be read from env vars
-DAYS_TO_CHECK = 7
-KEYWORD_FOR_LOOKUP = "FLORESTA"
+DAYS_TO_CHECK = os.getenv('DAYS_TO_CHECK') or 7
+KEYWORD_FOR_LOOKUP = os.getenv('KEYWORD_FOR_LOOKUP') or ''
 
 if __name__ == '__main__':
     process = CrawlerProcess(get_project_settings())
